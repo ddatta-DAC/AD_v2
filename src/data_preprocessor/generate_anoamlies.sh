b0fc2320a8b5408ea0f37fd5ce8d8197a10b1ab0
ddatta-DAC <ddatta@vt.edu>
2020-01-20 11:22:42 -0500

    update

---
+++ src/data_preprocessor/generate_anoamlies.sh
@@ -0,0 +1,5 @@
+python3 generate_anomalies_main.py --DIR  "china_export1"
+python3 generate_anomalies_main.py --DIR  "china_import1"
+python3 generate_anomalies_main.py --DIR  "us_import1"
+python3 generate_anomalies_main.py --DIR  "us_import2"
+
