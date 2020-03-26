78294892c87471db3ccff0f6fd23884201ad4aeb
Debanjan Datta <ddatta@vt.edu>
2020-03-26 06:54:34 -0400

    update

--- src/data_preprocessor/generate_anomalies.sh
+++ src/data_preprocessor/generate_anomalies.sh
@@ -1,6 +1,3 @@
-python3 generate_anomalies_main.py --DIR  us_import1
-python3 generate_anomalies_main.py --DIR  us_import2
-python3 generate_anomalies_main.py --DIR  us_import3
 python3 patterned_anomalies_generator_v0.py --DIR  us_import1
 python3 patterned_anomalies_generator_v0.py --DIR  us_import2
 python3 patterned_anomalies_generator_v0.py --DIR  us_import3
