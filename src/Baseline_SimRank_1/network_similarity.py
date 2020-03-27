0ea005763045bbbfc91926c6e581b8f87cf9e5d9
Debanjan Datta <ddatta@vt.edu>
2020-03-27 12:41:21 -0400

    update

--- src/Baseline_SimRank_1/network_similarity.py
+++ src/Baseline_SimRank_1/network_similarity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 # ---------------
 # Author : Debanjan Datta
@@ -13,6 +13,7 @@ import networkx as nx
 from networkx import simrank_similarity_numpy
 from collections import defaultdict
 import multiprocessing
+
 sys.path.append('./../..')
 sys.path.append('./..')
 from joblib import Parallel, delayed
@@ -23,11 +24,11 @@ try:
 except:
     from data_fetcher import data_fetcher_v2 as data_fetcher
 
-
 nodeObj_dict_file = 'nodeObj_dict.pkl'
-REFRESH_NODES  = False
+REFRESH_NODES = False
 id_col = 'PanjivaRecordID'
 nodeObj_Dict = None
+model_use_data_DIR = None
 
 
 # ------------------------------------------------------------3
@@ -39,13 +40,17 @@ def initialize(_dir, _model_use_data_DIR):
 
     DIR = _dir
     if _model_use_data_DIR is None:
-        model_use_data_DIR = './model_use_data'
+        model_use_data_DIR = 'model_use_data'
+    else:
+        model_use_data_DIR = _model_use_data_DIR
+
     if not os.path.exists(model_use_data_DIR):
         os.mkdir(model_use_data_DIR)
-    if not  os.path.exists(os.path.join(model_use_data_DIR,DIR)):
-        os.mkdir(os.path.join(model_use_data_DIR,DIR))
-    model_use_data_DIR = os.path.join(model_use_data_DIR,DIR)
+    if not os.path.exists(os.path.join(model_use_data_DIR, DIR)):
+        os.mkdir(os.path.join(model_use_data_DIR, DIR))
+    model_use_data_DIR = os.path.join(model_use_data_DIR, DIR)
     return
+
 
 # ------------------------------------------------------------
 def get_training_data(DIR):
@@ -64,47 +69,50 @@ def get_domain_dims(DIR):
         domain_dims = pickle.load(fh)
     return domain_dims
 
+
 # ------------------------------------
 class node:
     id = 0
+
     @staticmethod
     def get_id():
         _id = node.id
         node.id = node.id + 1
         return _id
 
-    def __init__(self, node_type , e_id):
-        self.node_type =  node_type
+    def __init__(self, node_type, e_id):
+        self.node_type = node_type
         self.id = node.get_id()
         self.e_id = e_id
 
 
-
 # Create the nodes first
 
-def create_transaction_nodes(list_record_ids, nodeObj_dict):
+def create_transaction_nodes(list_record_ids):
     global id_col
     global nodeObj_Dict
     domain = id_col
     nodeObj_Dict[domain] = defaultdict()
     for e_id in sorted(list_record_ids):
         node_obj = node(node_type=domain, e_id=e_id)
-        nodeObj_dict[domain][e_id] = node_obj
+        nodeObj_Dict[domain][e_id] = node_obj
     return
 
+
 def create_entity_nodes(domain_dims, nodeObj_dict):
-    for domain,size in domain_dims.items():
+    for domain, size in domain_dims.items():
         nodeObj_dict[domain] = defaultdict()
         for e_id in range(size):
-            node_obj = node( node_type=domain, e_id = e_id)
+            node_obj = node(node_type=domain, e_id=e_id)
             nodeObj_dict[domain][e_id] = node_obj
     return nodeObj_dict
+
 
 # ---------------------------
 # Use the training data to create nodes
 # Initially create entity nodes only
 # ---------------------------
-def get_base_nodeObj_dict (
+def get_base_nodeObj_dict(
         domain_dims,
         DIR
 ):
@@ -113,26 +121,28 @@ def get_base_nodeObj_dict (
     global nodeObj_dict_file
     global model_use_data_DIR
 
-
-
     f_path = os.path.join(model_use_data_DIR, nodeObj_dict_file)
-
+    print('Path :: ', f_path)
     if os.path.exists(f_path):
-        with open(f_path,'rb') as fh:
+        with open(f_path, 'rb') as fh:
             nodeObj_dict = pickle.load(fh)
             return nodeObj_dict
 
     nodeObj_dict = defaultdict()
     nodeObj_dict = create_entity_nodes(domain_dims, nodeObj_dict)
     with open(f_path, 'wb') as fh:
-        pickle.dump(nodeObj_dict, fh, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(
+            nodeObj_dict,
+            fh,
+            pickle.HIGHEST_PROTOCOL
+        )
     return nodeObj_dict
 
 
 # ------------------------------
 # Read in list of edge types
 # ------------------------------
-def read_schema( ):
+def read_schema():
     relations_list = []
     with open('schema.txt', 'r') as fh:
         lines = fh.readlines()
@@ -143,11 +153,11 @@ def read_schema( ):
             relations_list.append(_list)
     return relations_list
 
+
 def get_coOcc_dict(
         train_x_df,
         id_col='PanjivaRecordID'
 ):
-
     global model_use_data_DIR
     coOccDict_file = os.path.join(model_use_data_DIR, 'coOccDict.pkl')
     if os.path.exists(coOccDict_file):
@@ -164,20 +174,19 @@ def get_coOcc_dict(
     return coOccDict
 
 
+def save_graph(G, graph_file='nx_G0.pkl'):
+    global model_use_data_DIR
 
-
-def save_graph(G, graph_file = 'nx_G0.pkl' ):
-    global model_data_use_DIR
-
-    graph_obj_path = os.path.join(model_data_use_DIR,graph_file)
+    graph_obj_path = os.path.join(model_use_data_DIR, graph_file)
     nx.write_gpickle(G, graph_obj_path, protocol=4)
 
     return
 
-def read_graph( graph_file = 'nx_G1.pkl' ):
-    global model_data_use_DIR
+
+def read_graph(graph_file='nx_G1.pkl'):
+    global model_use_data_DIR
     graph_file = 'nx_G1.pkl'
-    graph_obj_path = os.path.join(model_data_use_DIR, graph_file)
+    graph_obj_path = os.path.join(model_use_data_DIR, graph_file)
     if os.path.exists(graph_obj_path):
         G = nx.read_gpickle(graph_obj_path)
         return G
@@ -191,15 +200,15 @@ def read_graph( graph_file = 'nx_G1.pkl' ):
 def get_initial_graph(
         df,
         domain_dims
-) :
+):
     global nodeObj_Dict
     graph_file = 'nx_G0.pkl'
 
-    G = read_graph(graph_file)
-    if G is not None:
-        return G
+    G0 = read_graph(graph_file)
+    if G0 is not None:
+        return G0
 
-    nodeObj_Dict = get_base_nodeObj_dict ( domain_dims, DIR)
+    nodeObj_Dict = get_base_nodeObj_dict(domain_dims, DIR)
     print(domain_dims)
 
     list_edge_types = read_schema()
@@ -214,7 +223,7 @@ def get_initial_graph(
 
     # Add edges
     n_jobs = multiprocessing.cpu_count()
-    from multiprocessing import  Pool
+    from multiprocessing import Pool
     from itertools import product
 
     for edge in list_edge_types:
@@ -222,7 +231,7 @@ def get_initial_graph(
         # get co_occurrence_matrix
         domain_1 = edge[0]
         domain_2 = edge[1]
-        if domain_1 > domain_2 :
+        if domain_1 > domain_2:
             domain_1, domain_2 = domain_2, domain_1
 
         key = domain_1 + '_+_' + domain_2
@@ -230,57 +239,59 @@ def get_initial_graph(
         domain_1_keys = list(nodeObj_Dict[domain_1].keys())
         domain_2_keys = list(nodeObj_Dict[domain_2].keys())
         args = []
-        for i_idx,j_idx in product(domain_1_keys,domain_2_keys):
+        for i_idx, j_idx in product(domain_1_keys, domain_2_keys):
             count = matrix[i_idx][j_idx]
-            if count == 0 : continue
-            args.append( [nodeObj_Dict[domain_1][i_idx], nodeObj_Dict[domain_2][j_idx],count])
+            if count == 0: continue
+            args.append([nodeObj_Dict[domain_1][i_idx], nodeObj_Dict[domain_2][j_idx], count])
 
         G.add_weighted_edges_from(args)
         print('Number of nodes and edges :: ', G.number_of_nodes(), G.number_of_edges())
         save_graph(G, graph_file)
         return G
 
-def add_transaction_nodes(df, G):
+
+def get_graph_W_transaction_nodes(G, df):
     global id_col
     global nodeObj_Dict
     graph_file = 'nx_G1.pkl'
-    G = read_graph(graph_file)
+    G1 = read_graph(graph_file)
 
-    if G is not None:
-        return G
+    if G1 is not None:
+        return G1
 
     list_record_ids = list(df[id_col])
     create_transaction_nodes(list_record_ids)
-    _list_obj =  list(nodeObj_Dict[id_col].values())
-    transactionID_2_serialID_dict = { e[0] : e[1] for e in enumerate(list(nodeObj_Dict[id_col].keys()),0)}
+    _list_obj = list(nodeObj_Dict[id_col].values())
+    transactionID_2_serialID_dict = {e[0]: e[1] for e in enumerate(list(nodeObj_Dict[id_col].keys()), 0)}
     G.add_nodes_from(_list_obj)
     list_edge_types = read_schema()
-    list_edge_types = [ _ for _ in list_edge_types if id_col in _ ]
+    list_edge_types = [_ for _ in list_edge_types if id_col in _]
 
     num_jobs = multiprocessing.cpu_count()
     for edge in list_edge_types:
+        print(edge)
         domain_1 = edge[0]
         domain_2 = edge[1]
-        tmp_df = df[[domain_1,domain_2]]
+        tmp_df = df[[domain_1, domain_2]]
 
-        def aux_func_01( row, domain_1,domain_2 ):
+        def aux_func_01(row, domain_1, domain_2):
             global nodeObj_Dict
+            print(row)
             i = nodeObj_Dict[domain_1][row[domain_1]]
             j = nodeObj_Dict[domain_2][row[domain_2]]
-            return (i,j)
+            G.add_egdes_from([i,j])
+        from pandarallel import pandarallel
+        pandarallel.initialize()
 
-        edge_list =  Parallel(num_jobs)(
-            delayed(aux_func_01)
-            (row,domain_1,domain_2,) for _,row in tmp_df.iterrows())
-        G.add_egdes_from(edge_list)
-    save_graph(G, graph_file  )
+        tmp_df.parallel_apply(
+            aux_func_01,
+            axis =1,
+            args=( domain_1, domain_2, )
+        )
+        # edge_list = Parallel(num_jobs)(
+        #     delayed(aux_func_01)
+        #     (row, domain_1, domain_2, ) for _, row in tmp_df.iterrows())
+        # print(edge_list)
+        # G.add_egdes_from(edge_list)
+    save_graph(G, graph_file)
     return G
-
-domain_dims = get_domain_dims(DIR)
-df = get_training_data(DIR, )
-G = get_initial_graph(df,domain_dims)
-
-
-
-
-
