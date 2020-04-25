aab4691750f75a3bfe33f9fc70e1e9a3c0608c86
ddatta-DAC <ddatta@vt.edu>
2020-04-24 20:00:36 -0400

    changes to data loading code

--- src/GAM/torch_data_loader.py
+++ src/GAM/torch_data_loader.py
@@ -19,112 +19,8 @@ from torch.utils.data import RandomSampler
 from torch import FloatTensor as FT
 from torch import LongTensor as LT
 
-class custom_dataset_type_1(Dataset):
-    def __init__(
-            self,
-            file_path=None,
-            is_numpy=False,
-            is_csv=True
-    ):
-        if is_numpy:
-            self.data = np.load(file_path)
-        if is_csv:
-            self.data = pd.read_csv(file_path, index_col=None)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        res = self.data.iloc[idx]
-        return np.array(res)
-
-
-# ------------------------------------------------- #
-# Given 2 data sets, retrieves samples from both in parallel
-# must provide the x cols and y cols for both
-# -------------------------------------------------- #
-class pair_Dataset(Dataset):
-    def __init__(
-            self,
-            df_1,
-            df_2,
-            x_cols,
-            y_col=None,
-            size_1=None,
-            size_2=None,
-            shuffle_prob=0.25
-    ):
-        self.data_1 = df_1
-        if size_1 is None:
-            size_1 = len(self.data_1)
-        else:
-            self.data_1 = self.data_1.head(size_1)
-
-        self.data_2 = df_2
-        if size_2 is None:
-            size_2 = len(self.data_2)
-        else:
-            self.data_2 = self.data_2.head(size_2)
-        self.size_1 = size_1
-        self.size_2 = size_2
-        self.x_cols = x_cols
-        self.y_col = y_col
-        self.shuffle_prob = shuffle_prob
-        return
-
-    def __len__(self):
-        return max(self.size_1, self.size_2)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        min_size = min(self.size_1, self.size_2)
-        try:
-            idx_s = [_ for _ in map(lambda x: x % min_size, idx)]
-        except:
-            idx_s = idx % min_size
-        shuffle_flag = np.random.uniform(0, 1) <= self.shuffle_prob
-
-        if self.size_1 < self.size_2:
-            if shuffle_flag:
-                np.random.shuffle(self.data_1.values)
-            idx_1 = idx_s
-            idx_2 = idx
-        elif self.size_1 > self.size_2:
-            if shuffle_flag:
-                np.random.shuffle(self.data_2.values)
-            idx_1 = idx
-            idx_2 = idx_s
-        else:
-            idx_1 = idx
-            idx_2 = idx
-
-        res_1_x = self.data_1[self.x_cols].iloc[idx_1]
-        res_2_x = self.data_2[self.x_cols].iloc[idx_2]
-
-        x1 = np.array(res_1_x)
-        x2 = np.array(res_2_x)
-        if self.y_col is not None:
-            try:
-                y1 = np.array(self.data_1[self.y_col].iloc[idx_1])
-            except:
-                y1 = None
-            try:
-                y2 = np.array(self.data_2[self.y_col].iloc[idx_2])
-            except:
-                y2 = None
-
-            return (x1, x2), (y1, y2)
-        else:
-            return x1, x2
-
-
 # ================================
-# This custom dataset can load single labelled or unlabelled dataset.
+# This custom data set can load single labelled or unlabelled dataset.
 # ================================
 class type1_Dataset(Dataset):
     def __init__(
@@ -227,14 +123,6 @@ class singleDataGenerator():
             return  x1
 
 
-
-data_LL_generator = pairDataGenerator(
-                df_1 = df_L,
-                df_2 = df_L,
-                x_cols=g_feature_cols,
-                y1_col= None,
-                y2_col= label_col,
-                batch_size=batch_size_r)
 
 # ------------------------------------------------- #
 # This generates a pair of (x1,[y1]), (x2,[y2])
@@ -364,41 +252,3 @@ class pairDataGenerator():
             return (x1, x2)
 
 
-
-def test():
-    f_path = './../../generated_data_v1/us_import1/test_data.csv'
-    dataset = custom_dataset_type_1(
-        f_path
-    )
-    import multiprocessing
-    num_proc = multiprocessing.cpu_count()
-    dataloader = DataLoader(
-        dataset,
-        batch_size=16,
-        shuffle=False,
-        num_workers=num_proc,
-        sampler=RandomSampler(dataset)
-    )
-
-    arr = []
-    for i_batch, sample_batched in enumerate(dataloader):
-        arr.extend(list(sample_batched[:, 0]))
-    print(len(arr))
-
-    # rs = RandomSampler(
-    #     dataset,
-    #     False,
-    #     100
-    # )
-    # for sample_batched in enumerate(rs):
-    #     print(sample_batched)
-
-    # dataloader1 = DataLoader(
-    #     ds,
-    #     batch_size=16,
-    #     shuffle=False,
-    #     num_workers=num_proc,
-    #     sampler=RandomSampler(ds)
-    # )
-
-# test()
