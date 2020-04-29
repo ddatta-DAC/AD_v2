79a60d37648274a532bc0d2542781876b2583bce
ddatta-DAC <ddatta@vt.edu>
2020-04-28 22:05:57 -0400

    updates

--- src/GAM/torch_data_loader.py
+++ src/GAM/torch_data_loader.py
@@ -18,6 +18,7 @@ from torch.utils.data import Dataset
 from torch.utils.data import RandomSampler
 from torch import FloatTensor as FT
 from torch import LongTensor as LT
+from itertools import islice
 
 # ================================
 # This custom data set can load single labelled or unlabelled dataset.
@@ -128,7 +129,7 @@ class singleDataGenerator():
 # If allow_refresh = False : return None on exhausting one data set
 # ------------------------------------------------- #
 
-class pairDataGenerator():
+class pairDataGenerator_v1():
     def __init__(
             self,
             df_1,
@@ -251,3 +252,180 @@ class pairDataGenerator():
             return (x1, x2)
 
 
+
+class pairDataGenerator_v2():
+
+    def __init__(
+            self,
+            df_1,
+            df_2,
+            x1_F_col,
+            x2_F_col,
+            x1_G_col,
+            x2_G_col,
+            y1_col=None,
+            y2_col=None,
+            batch_size=256,
+            device = None,
+            allow_refresh = False
+    ):
+        self.device = device
+        self.x1_F_col = x1_F_col
+        self.x2_F_col = x2_F_col
+        self.x1_G_col = x1_G_col
+        self.x2_G_col = x2_G_col
+        self.y1_col = y1_col
+        self.y2_col = y2_col
+        self.batch_size = batch_size
+        # Shuffle
+        self.df_1 = df_1.reindex(np.random.permutation(df_1.index))
+        self.df_2 = df_2.reindex(np.random.permutation(df_2.index))
+        self.index_1 = np.random.permutation(len(self.df_1))
+        self.index_2 = np.random.permutation(len(self.df_2))
+
+        self.iter_obj1 = self.get_indices_iter(self.index_1, batch_size, allow_refresh)
+        self.iter_obj2 = self.get_indices_iter(self.index_2, batch_size, allow_refresh)
+
+
+    def get_indices_iter(self, index_i, batch_size, allow_refresh):
+        def next_indices(
+                index_i,
+                batch_size,
+                refresh=False
+        ):
+            cur_idx = 0
+            while cur_idx + batch_size < len(index_i):
+                if refresh and cur_idx + batch_size >= len(index_i):
+                    cur_idx = 0
+                    np.random.shuffle(index_i)
+                    print('---')
+                yield index_i[cur_idx:cur_idx + batch_size]
+                cur_idx += batch_size
+
+        obj = iter(next_indices(index_i, batch_size, allow_refresh))
+        return obj
+
+
+    def get_next(self):
+        try:
+            next_1 = next( self.iter_obj1)
+        except:
+            next_1 = None
+
+        try:
+            next_2 = next( self.iter_obj2)
+        except:
+            next_2 = None
+
+        return next_1, next_2
+
+
+class pairDataGenerator_v2:
+
+    def __init__(
+            self,
+            df_1,
+            df_2,
+            x1_F_col,
+            x2_F_col,
+            x1_G_col,
+            x2_G_col,
+            y1_col=None,
+            y2_col=None,
+            batch_size=256,
+            device=None,
+            allow_refresh=False
+    ):
+        self.device = device
+        self.x1_F_col = x1_F_col
+        self.x2_F_col = x2_F_col
+        self.x1_G_col = x1_G_col
+        self.x2_G_col = x2_G_col
+        self.y1_col = y1_col
+        self.y2_col = y2_col
+        self.batch_size = batch_size
+        # Shuffle
+        self.df_1 = df_1.reindex(np.random.permutation(df_1.index))
+        self.df_2 = df_2.reindex(np.random.permutation(df_2.index))
+        index_1 = np.random.permutation(len(self.df_1))
+        index_2 = np.random.permutation(len(self.df_2))
+
+        self.iter_obj1 = self._get_indices_iter(index_1, batch_size, allow_refresh)
+        self.iter_obj2 = self._get_indices_iter(index_2, batch_size, allow_refresh)
+        return
+
+    def _get_indices_iter(self, index_i, batch_size, allow_refresh):
+        def next_indices(
+                index_i,
+                batch_size,
+                refresh=False
+        ):
+            cur_idx = 0
+            while True:
+                if cur_idx + batch_size > len(index_i):
+                    if allow_refresh:
+                        cur_idx = 0
+                        np.random.shuffle(index_i)
+                    else:
+                        print(cur_idx + batch_size)
+                        break
+
+                yield np.array(index_i[cur_idx:cur_idx + batch_size])
+                cur_idx += batch_size
+
+        obj = iter(next_indices(index_i, batch_size, allow_refresh))
+        return obj
+
+    def get_next(self):
+        try:
+            next_1_idx = next(self.iter_obj1)
+            print(next_1_idx)
+        except StopIteration:
+            next_1_idx = None
+
+        try:
+            next_2_idx = next(self.iter_obj2)
+        except StopIteration:
+            next_2_idx = None
+
+        if next_1_idx is None or next_2_idx is None:
+            return None
+
+        x1_F = None
+        x1_G = None
+        x2_F = None
+        x2_G = None
+        y1 = None
+        y2 = None
+
+        try:
+            x1_F = LT(self.df_1.loc[next_1_idx, self.x1_F_col].values).to(self.device)
+        except:
+            pass
+
+        try:
+            x1_G = LT(self.df_1.loc[next_1_idx, self.x1_G_col].values).to(self.device)
+        except:
+            pass
+
+        try:
+            x2_F = LT(self.df_2.loc[next_2_idx, self.x2_F_col].values).to(self.device)
+        except:
+            pass
+
+        try:
+            x2_G = LT(self.df_2.loc[next_2_idx, self.x2_G_col].values).to(self.device)
+        except:
+            pass
+
+        try:
+            y1 = FT(self.df_1.loc[next_1_idx, self.y1_col].values).to(self.device)
+        except:
+            pass
+
+        try:
+            y2 = FT(self.df_2.loc[next_2_idx, self.y2_col].values).to(self.device)
+        except:
+            pass
+
+        return (x1_F, x1_G, y1), (x2_F, x2_G, y2)
