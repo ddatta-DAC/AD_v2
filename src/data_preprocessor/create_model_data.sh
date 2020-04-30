6b179430ba9bac7815e85c04da3d2053608f7874
Debanjan Datta <ddatta@vt.edu>
2020-04-29 21:33:37 -0400

    updates

--- src/data_preprocessor/create_model_data.sh
+++ src/data_preprocessor/create_model_data.sh
@@ -1,3 +1,3 @@
-python3 create_model_data.py --DIR us_import1 --CREATE_MEAD_DATA
-python3 create_model_data.py --DIR us_import2 --CREATE_MEAD_DATA
-python3 create_model_data.py --DIR us_import3 --CREATE_MEAD_DATA
+python3 create_model_data.py --DIR us_import4 --CREATE_MEAD_DATA
+python3 create_model_data.py --DIR us_import5 --CREATE_MEAD_DATA
+python3 create_model_data.py --DIR us_import6 --CREATE_MEAD_DATA
